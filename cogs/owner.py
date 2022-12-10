cca7bf4f569d34f9c59d0241d2689f74c52dbcec
Yonipy <itz.kqlvin@gmail.com>
2022-12-10 18:06:45 -0500

    eee

--- cogs/owner.py
+++ cogs/owner.py
@@ -48,7 +48,7 @@ class owner(commands.Cog):
         for page in paginator.pages:
             await ctx.send(page)
 
-    @tasks.loop(seconds=1800)
+    @tasks.loop(seconds=10)
     async def steps(self):
         channel = self.bot.get_channel(1051145679382253628)
         random_steps = str(random.randint(15000, 19500))
