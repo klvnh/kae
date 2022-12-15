379d797290a7f9f22c7b010dd15bef4b85704d1c
Yonipy <itz.kqlvin@gmail.com>
2022-12-14 23:15:58 -0500

    updated!

--- cogs/spotify.py
+++ cogs/spotify.py
@@ -49,29 +49,33 @@ class Spotify(commands.Cog):
         after_custom_activity = discord.utils.find(
             lambda activity: isinstance(activity, discord.CustomActivity),
             after.activities)    
-        
-        # check any other changes and return.
-        if before.status != after.status or before_activity != after_activity or before_custom_activity != after_custom_activity:
-            return
 
-        # when user stops listening.
-        if after_spotify is None or before_spotify is None and after_spotify is None:
-            self.bot.dispatch("websocket_update", "STOPPED")
-            return
-
-        # when user starts listening.
-        if before_spotify is None and after_spotify is not None:
-            self.bot.dispatch("websocket_update", "STARTED")
-
-        # when user changes songs.
-        if before_spotify is None:
-            return
-        elif before_spotify.created_at != after_spotify.created_at:
-            self.bot.dispatch("websocket_update", "SKIPPED")
 
         if before_spotify is None:
             return
         else:
+
+            # check any other changes and return.
+            if before.status != after.status or before_activity != after_activity or before_custom_activity != after_custom_activity:
+                return
+
+            # when user stops listening.
+            if after_spotify is None or before_spotify is None and after_spotify is None:
+                self.bot.dispatch("websocket_update", "STOPPED")
+                return
+
+            # when user starts listening.
+            if before_spotify is None and after_spotify is not None:
+                self.bot.dispatch("websocket_update", "STARTED")
+
+            # when user forwards the song.
+            if before_spotify.created_at != after_spotify.created_at:
+                self.bot.dispatch("websocket_update", "FORWARD")
+
+            # when user skips the songs.
+            if before_spotify.title != after_spotify.title:
+                self.bot.dispatch("websocket_update", "SKIPPED") 
+
             try:
                 if self.playing_cache[before.id]['song'] == before_spotify.title:
                     if self.playing_cache[before.id]['song'] != before_spotify.title:
@@ -84,20 +88,14 @@ class Spotify(commands.Cog):
                 self.playing_cache[before.id] = {'song': after_spotify.title, 'guild_id': after.guild.id}
                 print('just started', self.playing_cache)
 
-        minutes = int(round(after_spotify.duration.total_seconds()) / 60)
-        format = ' '.join(after_spotify.artists)
-        embed = discord.Embed(color = 0x2F3136, timestamp=datetime.datetime.now())
-        embed.set_author(name='NOW PLAYING.' + ('\u2800' * 19))
-        embed.set_footer(text=f'{after.name}#{after.discriminator}', icon_url=after.avatar.url)
-        embed.add_field(name=after_spotify.title, value=format, inline=False)
-        embed.add_field(name='Duration', value=f"Est. {minutes} minute(s)", inline=False)
-        embed.set_thumbnail(url=after_spotify.album_cover_url)
-        for channel in after.guild.text_channels:
-            if channel.name.startswith('np'):
-                await channel.send(embed=embed, view=Spotify_Select_View(after_spotify.track_url))
-        
-
-    @app_commands.command(description='Sets the current channel as the now playing channel.')
-    async def set_channel(self, interaction):
-        self.bot.ids.append(interaction.channel.id)
-        await interaction.response.send_message(f'{interaction.channel.mention} has been set as channel.')
+            minutes = int(round(after_spotify.duration.total_seconds()) / 60)
+            format = ' '.join(after_spotify.artists)
+            embed = discord.Embed(color = 0x2F3136, timestamp=datetime.datetime.now())
+            embed.set_author(name='NOW PLAYING.' + ('\u2800' * 19))
+            embed.set_footer(text=f'{after.name}#{after.discriminator}', icon_url=after.avatar.url)
+            embed.add_field(name=after_spotify.title, value=format, inline=False)
+            embed.add_field(name='Duration', value=f"Est. {minutes} minute(s)", inline=False)
+            embed.set_thumbnail(url=after_spotify.album_cover_url)
+            for channel in after.guild.text_channels:
+                if channel.name.startswith('np'):
+                    await channel.send(embed=embed, view=Spotify_Select_View(after_spotify.track_url))
