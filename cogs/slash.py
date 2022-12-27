5c366f1e53d39b233f5bfebf2519e0ce01b19952
Yonipy <itz.kqlvin@gmail.com>
2022-12-27 14:57:56 -0500

    eeee

--- cogs/slash.py
+++ cogs/slash.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import datetime
 
 async def setup(bot):
     await bot.add_cog(Slash(bot))
@@ -15,4 +16,4 @@ class Slash(commands.Cog):
 
     @app_commands.command(name='hello', description='A default slash command.')
     async def hello_slash(self, interaction):
-        await interaction.response.send_message('Hello')
+        await interaction.response.send_message(f'Hello {interaction.user.id}, Invoked at {datetime.datetime.now()}')
